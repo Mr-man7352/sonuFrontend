37192cfb599c13af4d69b484864307e049b3c774
Manish Sahu <manissh7352@gmail.com>
2022-01-09 21:11:59 +0530

    added nav bar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -11,4 +11,26 @@ export class NavbarComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  ngAfterContentInit() {
+    (() => {
+      let nav = document.getElementById('nav');
+      console.log(nav);
+
+      if (nav == null) {
+        return;
+      }
+
+      window.addEventListener('scroll', () => {
+        if (window.scrollY > 1.5 && window.innerWidth > 992) {
+          nav.classList.add('nav-sticky');
+          document.body.style.paddingTop = '70';
+        } else if (window.innerWidth < 992) {
+          nav.classList.add('nav-sticky');
+        } else {
+          nav.classList.remove('nav-sticky');
+        }
+      });
+      console.log('Listner added');
+    })();
+  }
 }
