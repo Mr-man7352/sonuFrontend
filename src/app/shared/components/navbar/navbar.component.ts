ddc4f3266462be5aabc593b121069bc307ecd364
Manish Sahu <manissh7352@gmail.com>
2022-01-09 23:31:32 +0530

    navbar added

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,35 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { faCoffee, faTaxi } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCoffee,
+  faTaxi,
+  faMobileAlt,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  // icons
   faCoffee = faCoffee;
   faTaxi = faTaxi;
+  faMobileAlt = faMobileAlt;
+
+  //
   constructor() {}
 
   ngOnInit(): void {}
   ngAfterContentInit() {
     (() => {
-      let nav = document.getElementById('nav');
-      console.log(nav);
+      // let nav = document.getElementById('nav');
+      // console.log(nav);
 
-      if (nav == null) {
-        return;
-      }
+      // if (nav == null) {
+      //   return;
+      // }
 
-      window.addEventListener('scroll', () => {
-        if (window.scrollY > 1.5 && window.innerWidth > 992) {
-          nav.classList.add('nav-sticky');
-          document.body.style.paddingTop = '70';
-        } else if (window.innerWidth < 992) {
-          nav.classList.add('nav-sticky');
-        } else {
-          nav.classList.remove('nav-sticky');
-        }
-      });
       console.log('Listner added');
     })();
   }
