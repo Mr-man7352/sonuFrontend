c256048f6453099fc783ebec860d303f3fb67f6a
Manish Sahu <manissh7352@gmail.com>
2022-01-15 20:50:38 +0530

    added footer, completed homepage

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  faMapMarker,
+  faPhone,
+  faEnvelope,
+  faTaxi,
+  faCaretRight,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  faMapMarker = faMapMarker;
+  faPhone = faPhone;
+  faEnvelope = faEnvelope;
+  faTaxi = faTaxi;
+  faCaretRight = faCaretRight;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
